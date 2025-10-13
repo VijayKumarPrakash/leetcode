7422b772889405191bea1db8481715394e6fe4fc
Vijay Kumar Prakash <vijaykumar.sb.22@gmail.com>
2025-10-13 00:43:31 -0700

    Optimize asteroid collision code

--- 735_asteroid_collision.py
+++ 735_asteroid_collision.py
@@ -2,23 +2,27 @@ class Solution:
     def asteroidCollision(self, asteroids):
         stack = []
 
-        def sign(num):
-            return "pos" if num > 0 else "neg"
-
         for rock in asteroids:
-            stack.append(rock)
-
             # If sign difference exists, repeat until stability
             # It's okay if the left asteroid is moving left, and the right asteroid is moving right
-            while len(stack) > 1 and sign(stack[-1]) == "neg" and sign(stack[-2]) == "pos":
-                if abs(stack[-1]) > abs(stack[-2]):
-                    stack[-2] = stack[-1]
-                elif abs(stack[-1]) == abs(stack[-2]):
+            while stack and rock < 0 < stack[-1]:
+                if abs(stack[-1]) > abs(rock):
+                    break
+                elif abs(stack[-1]) < abs(rock):
                     stack.pop()
-                stack.pop()                
+                else:
+                    # They are equal
+                    stack.pop()
+                    break
+            else:
+                stack.append(rock)
         
         return stack
 
 soln = Solution()
-final_state = soln.asteroidCollision([5, 10, -5])
+# input_asteroids = [10, 2, -5]
+# input_asteroids = [8,-8]
+# input_asteroids = [8,-3,4,6,8,-4,-2,6,-12,12,6,-6]
+input_asteroids = [-100, -12, -13, -5, -7, -10, 5, 17, -6, 3, -8, 2, -10, 4, -9]
+final_state = soln.asteroidCollision(input_asteroids)
 print(final_state) 
