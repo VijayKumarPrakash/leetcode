f0aac26d7066f07109140d9d19685bf1699018d0
Vijay Kumar Prakash <vijaykumar.sb.22@gmail.com>
2026-03-01 20:55:22 -0800

    Optimize Longest substring without repeating characters

--- 3_longest_substring_without_repeating_characters.py
+++ 3_longest_substring_without_repeating_characters.py
@@ -4,29 +4,26 @@ class Solution:
         if s_len == 0 or s_len == 1:
             return s_len
 
-        left, right = 0, 1
-        current = set(s[left])
-        max_length = 1
+        left, right = 0, 0
+        tracker = {}
+        max_length = 0
 
         while right < s_len:
             c = s[right]
 
-            if c not in current:
-                current.add(c)
-                right += 1
+            if c not in tracker:
+                tracker[c] = right
 
             else:
-                # Slide
-                while s[left] != s[right]:
-                    current.remove(s[left])
-                    left += 1
-                current.remove(s[left])
-                left += 1
-                right += 1
-                current.add(c)
+                # Slide, but only slide left if the duplicate is within the current window
+                if tracker[c] >= left:
+                    left = tracker[c] + 1
             
+            tracker[c] = right
+            right += 1
+
             # Update max_length if needed
-            new_length = len(current)
+            new_length = right - left
             max_length = max(max_length, new_length)
 
 
