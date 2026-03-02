9492f6bb8447c8bbb8be6d218ecaf91d4f8fe6b9
Vijay Kumar Prakash <vijaykumar.sb.22@gmail.com>
2026-03-01 16:49:21 -0800

    Solve Two Sum

---
+++ 1_two_sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        dt = dict()
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in dt:
+                return [i, dt[complement]]
+            dt[num] = i
+
+# Example usage:
+sol = Solution()
+print(sol.twoSum([2, 7, 11, 15], 9))  # Output: [0, 1]
+print(sol.twoSum([3, 2, 4], 6))       # Output: [1, 2]
+print(sol.twoSum([3, 3], 6))          # Output: [0, 1]
